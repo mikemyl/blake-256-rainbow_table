3ec133d85a984b8daa26a6483f9346b6c4f64a8e
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2016-05-21 15:46:41 +0300

    Added hash_lookup function

--- sorting_function.py
+++ sorting_function.py
@@ -1,16 +1,25 @@
 """Sorting Hex Values."""
 import csv
-import operator
+
+
+def hash_lookup(mydict):
+    """Lookup Function."""
+    while True:    # infinite loop
+        input_u = raw_input("\n\Give the hash: ")
+        if input_u in mydict:
+            print("Success!!")
+            print mydict[input_u]
+        else:
+            print("Not Found")
 
 
 def main():
     """The main function of my script."""
     # Read lines from file
-    output_list = []
     with open("rainbow.csv", "r") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
-        mydict = dict((rows[0], rows[1]) for rows in spamreader)
-
+        mydict = dict((rows[1], rows[0]) for rows in spamreader)
+        hash_lookup(mydict)
 
 if __name__ == "__main__":
     main()
