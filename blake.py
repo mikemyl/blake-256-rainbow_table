19444d660f2ce2c2c1c9a442063bb0402673c21b
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2016-05-21 12:50:20 +0300

    Fix pep8 - pep256

--- blake.py
+++ blake.py
@@ -1,28 +1,39 @@
+"""Mike's script."""
 from blake_wrapper import BLAKE
 from binascii import hexlify, unhexlify
 from random import randint
 import csv
 
-char_set = list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@')
+char_set = list(
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@')
 pass_length = 6
 
+
 def get_random_pass():
+    """Get random pass."""
     rand_chars = []
     for index in range(pass_length):
         rand_chars.append(char_set[randint(0, 63)])
     return ''.join(rand_chars)
 
+
 def reduce(hash_val):
+    """Reduce function."""
     new_pass = []
     for index in range(pass_length):
-        new_pass.append(char_set[int(hash_val[(index*2) : (index*2+2)], 16) % 64])
+        new_pass.append(char_set[
+            int(hash_val[(index*2): (index*2+2)], 16) % 64])
     return ''.join(new_pass)
 
+
 def hash(text):
+    """Hash function."""
     blake = BLAKE(256).final(text)
     return hexlify(blake)
 
+
 def create_chain(chain_length, csv_writer):
+    """Create chain."""
     passwd = get_random_pass()
     hashed_passwd = hash(passwd)
     for index in range(chain_length):
@@ -31,7 +42,7 @@ def create_chain(chain_length, csv_writer):
     csv_writer.writerow([passwd, hashed_passwd])
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     csv_file = open('rainbow.csv', 'wb')
     wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
     for index in range(10):
